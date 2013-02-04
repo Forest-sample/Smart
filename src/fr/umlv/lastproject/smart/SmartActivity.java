f122d70c49ab9f0ef29591fa4819a1799c671244
tbrun820 <tbrun@etudiant.univ-mlv.fr>
2013-02-05 00:33:29 +0100

    first

---
+++ src/fr/umlv/lastproject/smart/SmartActivity.java
@@ -0,0 +1,22 @@
+package fr.umlv.lastproject.smart;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SmartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_smart);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_smart, menu);
+		return true;
+	}
+
+}
