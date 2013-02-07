dc5ee4210824feca5c1b0973fd7b587419c3d890
tbrun820 <tbrun@etudiant.univ-mlv.fr>
2013-02-07 15:11:16 +0100

    maj du pom

--- src/fr/umlv/lastproject/smart/SmartActivity.java
+++ src/fr/umlv/lastproject/smart/SmartActivity.java
@@ -10,6 +10,7 @@ public class SmartActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_smart);
+		System.out.println();
 	}
 
 	@Override
