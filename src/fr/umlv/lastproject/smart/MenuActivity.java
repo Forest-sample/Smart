1190fe617fb9e6ee0e5bfcdac7ca9c758b6fcc39
tbrun820 <tbrun@etudiant.univ-mlv.fr>
2013-02-11 00:14:20 +0100

    Modification du nom

--- src/fr/umlv/lastproject/smart/MenuActivity.java
+++ src/fr/umlv/lastproject/smart/MenuActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class SmartActivity extends Activity {
+public class MenuActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
