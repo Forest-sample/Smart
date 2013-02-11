d5d8122ab6bca5ec7a86f2d68789e994ab664d10
tbrun820 <tbrun@etudiant.univ-mlv.fr>
2013-02-11 16:31:35 +0100

    MyPositonOverlay : ok

--- src/fr/umlv/lastproject/smart/MenuActivity.java
+++ src/fr/umlv/lastproject/smart/MenuActivity.java
@@ -1,13 +1,17 @@
 package fr.umlv.lastproject.smart;
 
 
+import org.metalev.multitouch.controller.MultiTouchController.PositionAndScale;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.OverlayManager;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 public class MenuActivity extends Activity {
@@ -23,14 +27,17 @@ public class MenuActivity extends Activity {
 	private MapView mapView ;
 	private MapController mapController ;
 	private OverlayManager overlayManager;
-
-
+	private MyPositionOverlay myPositionOverlay ;
+	private GPS gps ;
+	private GeoPoint lastPosition ;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_smart);
 		initMap();
+		initGps();
 
 	}
 
@@ -48,12 +55,33 @@ public class MenuActivity extends Activity {
 		mapView = (MapView) findViewById(R.id.mapview) ;
 		mapController = mapView.getController() ;
 		overlayManager = mapView.getOverlayManager();
+		myPositionOverlay = new MyPositionOverlay(this);
 		mapView.setTileSource(TileSourceFactory.MAPNIK);
 		mapView.setClickable(true);
 		mapView.setMultiTouchControls(true);
 		mapController.setZoom(15);
 		overlayManager.add(new ScaleBarOverlay(this));
-
+		overlayManager.add(myPositionOverlay);
+		
 	}
+
+	/**
+	 * This methode is use to connect the gps to the positionOverlay
+	 */
+	public void initGps(){
+		gps = new GPS((LocationManager) getSystemService(Context.LOCATION_SERVICE)) ;
+		gps.start(5000, 10);
+		gps.addGPSListener(new IGPSListener() {
+			
+			@Override
+			public void actionPerformed(GPSEvent event) {
+				myPositionOverlay.setAccurency(event.getAccuracy());
+				myPositionOverlay.setLatitiude(event.getLatitude()) ;
+				myPositionOverlay.setLongitude(event.getLongitude()) ;
+				mapController.setCenter(new GeoPoint(event.getLatitude(), gps.getLongitude()));
+			}
+		});
+	}
+	
 
 }
