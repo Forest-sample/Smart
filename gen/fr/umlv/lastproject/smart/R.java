86fbb61bfecf89b3fb0a276330a14d71bb92c385
tfaddoul <tfaddoul@etudiant.univ-mlv.fr>
2013-02-11 14:25:23 +0100

    Classes GPS / GPSListener / GPSEvent : OK

--- gen/fr/umlv/lastproject/smart/R.java
+++ gen/fr/umlv/lastproject/smart/R.java
@@ -15,7 +15,12 @@ public final class R {
         public static final int smart=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int altitude=0x7f070003;
+        public static final int latitude=0x7f070001;
+        public static final int longitude=0x7f070002;
+        public static final int menu_settings=0x7f070005;
+        public static final int precision=0x7f070004;
+        public static final int table=0x7f070000;
     }
     public static final class layout {
         public static final int activity_smart=0x7f030000;
